e4f24455717eb2e43c9d7b729067d7a7093aa663
ashutosh <ashutoshb@gmail.com>
2018-04-03 22:52:01 +0200

    disable debug for gunicorn

--- compose/local/django/start.sh
+++ compose/local/django/start.sh
@@ -15,7 +15,7 @@ elif [ "$ENV" == "prd" ]
 then
     python /app/manage.py collectstatic --noinput
     python /app/manage.py migrate
-    /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:8000 --log-level debug --chdir=/app
+    /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:8000 --chdir=/app
 else
     echo "Invalid ENV variable"
     exit
