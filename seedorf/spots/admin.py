6fba104380b0fce4ddbfd948c2e898559711e1a6
ashutosh <ashutoshb@gmail.com>
2018-04-18 14:25:11 +0200

    add spot images, amenities, opening times to django admin

--- seedorf/spots/admin.py
+++ seedorf/spots/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Spot
+from .models import Spot, SpotOpeningTime, SpotImage, SpotAmenity
 
 admin.site.register(Spot)
+admin.site.register(SpotOpeningTime)
+admin.site.register(SpotImage)
+admin.site.register(SpotAmenity)
