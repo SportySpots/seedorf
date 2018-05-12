cef5c7d07420ed4ce75531e95c61021ea26ceb08
ashutosh <ashutoshb@gmail.com>
2018-05-12 21:18:58 +0200

    remove django admin url setting

--- config/settings/prd.py
+++ config/settings/prd.py
@@ -227,8 +227,5 @@ RAVEN_CONFIG = {
     'DSN': SENTRY_DSN
 }
 
-# Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL')
-
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
