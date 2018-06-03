f5e90732a6d7d5f878f88b810f744eb7c11fdafc
ashutosh <ashutoshb@gmail.com>
2018-06-03 22:27:32 +0200

    send emails with postmark

--- config/settings/base.py
+++ config/settings/base.py
@@ -414,7 +414,7 @@ LOGOUT_ON_PASSWORD_CHANGE = True
 # Allauth settings
 # REF: https://django-allauth.readthedocs.io/en/latest/configuration.html
 # ------------------------------------------------------------------------------
-ACCOUNT_ADAPTER = 'allauth.account.adapter.DefaultAccountAdapter'
+ACCOUNT_ADAPTER = 'seedorf.users.adapter.DefaultAccountAdapter'
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = False
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
--- seedorf/users/adapters.py
+++ seedorf/users/adapters.py
@@ -1,13 +1,52 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
+from django.core.mail import EmailMessage
 
 
 class AccountAdapter(DefaultAccountAdapter):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     def is_open_for_signup(self, request):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
 
+    def send_confirmation_mail(self, request, emailconfirmation, signup):
+        activate_url = self.get_email_confirmation_url(request, emailconfirmation)
+
+        ctx = {
+            "user": emailconfirmation.email_address.user,
+            "product_name": "SportySpots",
+            "first_name": emailconfirmation.email_address.user.first_name,
+            "product_url": "https://www.sportyspots.com",
+            "activate_url": activate_url,
+            "support_email": "info@sportyspots.com",
+            "sender_name": "SportySpots",
+            "company_name": "SportySpots",
+            "company_address": "Amsterdam, The Netherlands",
+            "key": emailconfirmation.key,
+        }
+
+        message = EmailMessage(subject=None, to=[emailconfirmation.email_address.email])
+
+        if signup:
+            # TODO: Define template ids in settings
+            message.template_id = 6756321  # use this Postmark template
+        else:
+            # TODO: Create a postmark template for email confirmation
+            # email_template = 'account/email/email_confirmation'
+            message.template_id = 6756321  # use this Postmark template
+
+        message.merge_global_data = ctx
+
+        message.send()
+
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     def is_open_for_signup(self, request, sociallogin):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
