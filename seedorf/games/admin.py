764bcea85647de261e3f884e9dd4f4c6386e50a6
ashutosh <ashutoshb@gmail.com>
2018-06-08 00:40:30 +0200

    update game django admin list view

--- seedorf/games/admin.py
+++ seedorf/games/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Game, RsvpStatus
 
-admin.site.register(Game)
+
+class GameAdmin(admin.ModelAdmin):
+    readonly_fields = ('uuid', 'created_at', 'modified_at',)
+    list_display = ('uuid', 'name', 'start_time', 'end_time')
+
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(RsvpStatus)
