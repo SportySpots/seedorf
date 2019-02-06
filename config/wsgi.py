8408de77ec1067891571848350333c5ed44c1a80
ashutosh <ashutoshb@gmail.com>
2019-02-06 20:34:39 +0100

    fix wsgi application path

--- config/wsgi.py
+++ config/wsgi.py
@@ -20,7 +20,7 @@ from django.core.wsgi import get_wsgi_application
 
 # This allows easy placement of apps within the interior
 # seedorf directory.
-app_path = os.path.dirname(os.path.abspath(__file__)).replace("/config", "")
+app_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 sys.path.append(os.path.join(app_path, "seedorf"))
 
 if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.prd":
