1c770b715491616513da11b9f2b5b4c42a4f7be4
Tom Klaver <tomklav@gmail.com>
2019-02-13 11:40:32 +0100

    Enable facebook social auth provider

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,7 +60,7 @@ THIRD_PARTY_APPS = [
     "allauth",  # registration
     "allauth.account",  # registration
     "allauth.socialaccount",  # registration
-    # "allauth.socialaccount.providers.facebook",
+    "allauth.socialaccount.providers.facebook",
     # "allauth.socialaccount.providers.twitter",
     "allauth.socialaccount.providers.google",
     "crispy_forms",  # Form layouts
