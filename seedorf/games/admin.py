16ab30014eba8479a9232ba0754c2c7cea6f1767
ashutosh <ashutoshb@gmail.com>
2019-03-07 00:09:56 +0100

    rearrange game admin

--- seedorf/games/admin.py
+++ seedorf/games/admin.py
@@ -8,10 +8,23 @@ class RsvpStatusAdmin(admin.TabularInline):
 
 
 class GameAdmin(admin.ModelAdmin):
-    readonly_fields = ("uuid", "created_at", "modified_at")
+    readonly_fields = ("uuid", "created_at", "modified_at", "deleted_at")
     list_display = ("uuid", "name", "sport", "status", "start_time", "end_time")
-    list_filter = ("status",)
+    list_filter = ("status", "invite_mode")
     inlines = [RsvpStatusAdmin]
+    fieldsets = (
+        (None, {"fields": ("uuid", "name", "organizer", "sport", "spot", "status", "capacity", "description")}),
+        (
+            "Activity Time",
+            {"classes": ("expand",), "fields": ("start_time", "start_timezone", "end_time", "end_timezone")},
+        ),
+        ("RSVP Time", {"classes": ("collapse",), "fields": ("rsvp_open_time", "rsvp_close_time", "rsvp_closed")}),
+        (
+            "Visibility",
+            {"classes": ("collapse",), "fields": ("show_remaining", "is_listed", "is_shareable", "is_featured")},
+        ),
+        ("Audit", {"classes": ("collapse",), "fields": ("created_at", "modified_at", "deleted_at")}),
+    )
 
 
 admin.site.register(Game, GameAdmin)
