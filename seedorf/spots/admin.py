06afaf5478dc16d799932dcd20b60a40fc5a933e
ashutosh <ashutoshb@gmail.com>
2019-03-07 01:26:37 +0100

    refactor spots admin

--- seedorf/spots/admin.py
+++ seedorf/spots/admin.py
@@ -17,9 +17,35 @@ class SpotAmenityAdmin(admin.StackedInline):
 
 class SpotAdmin(admin.ModelAdmin):
     search_fields = ["name"]
-    list_display = ("name", "owner", "modified_at")
+    readonly_fields = ("uuid", "created_at", "modified_at", "deleted_at")
+    list_display = ("name", "owner", "is_verified", "is_permanently_closed", "is_public", "is_temporary", "modified_at")
     list_filter = ("sports__category", "is_verified", "is_public")
     inlines = [SpotImageAdmin, SpotOpeningTimeAdmin, SpotAmenityAdmin]
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "uuid",
+                    "name",
+                    "slug",
+                    "owner",
+                    "address",
+                    "description",
+                    "logo",
+                    "homepage_url",
+                    "is_verified",
+                    "is_permanently_closed",
+                    "is_public",
+                    "is_temporary",
+                    "establishment_date",
+                    "closure_date",
+                    "sports",
+                )
+            },
+        ),
+        ("Audit", {"classes": ("collapse",), "fields": ("created_at", "modified_at", "deleted_at")}),
+    )
 
 
 admin.site.register(Spot, SpotAdmin)
