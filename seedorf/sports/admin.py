c54db88d1183e95bfcfa0377a70a5dd43cc822cb
ashutosh <ashutoshb@gmail.com>
2019-03-07 01:34:57 +0100

    refactor sport admin

--- seedorf/sports/admin.py
+++ seedorf/sports/admin.py
@@ -4,9 +4,10 @@ from .models import Sport
 
 
 class SportAdmin(admin.ModelAdmin):
-    readonly_fields = ("uuid", "created_at", "modified_at")
-    fields = ("uuid", "category", "name", "description", "translations", "created_at", "modified_at")
+    readonly_fields = ("uuid", "created_at", "modified_at", "deleted_at")
+    fields = ("uuid", "category", "name", "description", "translations", "created_at", "modified_at", "deleted_at")
     list_display = ("uuid", "category", "name")
+    list_filter = ("category",)
 
 
 admin.site.register(Sport, SportAdmin)
