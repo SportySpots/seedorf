a04cd429982568c3720a242612fe3cdd1afea0f9
ashutosh <ashutoshb@gmail.com>
2019-03-13 18:06:15 +0100

    add additional s3 url to imgix transformation

--- seedorf/utils/templatetags/tags.py
+++ seedorf/utils/templatetags/tags.py
@@ -9,4 +9,7 @@ register = template.Library()
 def imgix_url(value):
     if value.startswith("https://s3.amazonaws.com/sportyspots-prd"):
         return value.replace("https://s3.amazonaws.com/sportyspots-prd", "https://sportyspots.imgix.net")
+    elif value.startswith("https://sportyspots-prd.s3.amazonaws.com"):
+        return value.replace("https://sportyspots-prd.s3.amazonaws.com", "https://sportyspots.imgix.net")
+
     return value
