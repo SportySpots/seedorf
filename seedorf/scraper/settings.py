3f09c720ea8df0d1171022b68b494c2758f75d44
ashutosh <ashutoshb@gmail.com>
2019-12-21 23:32:36 +0100

    fix scraper settings

--- seedorf/scraper/settings.py
+++ seedorf/scraper/settings.py
@@ -11,8 +11,8 @@
 
 BOT_NAME = "sportyscraper"
 
-SPIDER_MODULES = ["sportyscraper.spiders"]
-NEWSPIDER_MODULE = "sportyscraper.spiders"
+SPIDER_MODULES = ["spiders"]
+NEWSPIDER_MODULE = "spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
